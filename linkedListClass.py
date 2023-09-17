33ac89d9e59d066584692092c4d1957a6039b320
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-09-17 19:30:08 +0530

    linked list tests

--- linkedListClass.py
+++ linkedListClass.py
@@ -2,47 +2,46 @@ class Node:
     def __init__(self, value) -> None:
         self.value = value
         self.next = None
-        
-
+    
 class LinkedList:
-    def __init__(self, value) -> None:
+    def __init__(self,value) -> None:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
     
-    def apnd(self, value):
+    def append(self,value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
-        else :
+        else:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
     
-    def pops(self):
-        temp = self.head
-        if self.length == 0:
+    def pop(self):
+        if self.head is None :
             return None
-        while temp.next:
-            pre = temp
+        temp = self.head
+        while temp:
+            pre = temp 
             temp = temp.next
         self.tail = pre
         self.tail.next = None
         self.length -= 1
-        if self.length == 0 :
+        if self.length == 0:
             self.head = None
             self.tail = None
-            
+        
     def print_lists(self):
-        temp = self.head
-        while temp:
+        temp = self.head 
+        while temp.next:
             print(temp.value)
             temp = temp.next
-        
-mylist = LinkedList(1)
-mylist.apnd('3434')
-mylist.apnd('786')
-mylist.pops()
+    
+mylist = LinkedList(2)
+mylist.append('345554')
+mylist.append(786)
+mylist.pop()
 mylist.print_lists()
