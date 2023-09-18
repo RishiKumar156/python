eff7966a310575fc5bf0772b44bfb607488a9c69
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-09-18 13:08:40 +0530

    new Linked List

--- lktest.py
+++ lktest.py
@@ -1,27 +1,27 @@
 class Node:
-    def __init__(self,value) -> None:
+    def __init__(self,value):
         self.value = value
         self.next = None
     
-class LinkedList():
-    def __init__(self, value) -> None:
+class LinkedList:
+    def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        self.length = 1
+        self.length = 0
     
     def append(self,value):
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
-        else:
+        else :
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-    
+        
     def pop(self):
-        temp = self.head
+        temp = self.head 
         pre = temp
         if self.length == 0:
             return None
@@ -32,34 +32,37 @@ class LinkedList():
         self.tail.next = None
         self.length -= 1
         
+    def pop_first(self):
+        if self.length == 0:
+            self.tail = None
+            return None
+        temp = self.head 
+        self.head = self.head.next 
+        temp.next = None
+        self.length -= 1
+        if self.head is None:
+            self.tail = None
+            
     def prepend(self,value):
         new_node = Node(value)
-        if self.length ==0:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         new_node.next = self.head
         self.head = new_node
         self.length += 1
         
-    def pop_first(self):
-        if self.length is None:
-            return False
-        self.head.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.tail = None
-    
     def print_nodes(self):
-        temp = self.head 
+        temp = self.head
         while temp:
             print(temp.value)
-            temp = temp.next 
-    
-    
+            temp = temp.next
+            
 test = LinkedList(1)
 test.append(2)
 test.append(3)
 test.append(4)
-test.pop_first()
+test.prepend(0)
 test.pop()
+test.pop_first()
 test.print_nodes()
