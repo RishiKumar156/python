5d676b2a4c3b1872be624312dcb3024b6b443795
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-09 22:35:06 +0530

    pandas data migration test

--- migratecsvdata.py
+++ migratecsvdata.py
@@ -1,18 +1,10 @@
+import pandas as ps 
 
+file1 = ps.read_csv('zoho_auths.csv')
+file2 = ps.read_csv('zoho_auths1.csv')
 
-import pandas as pd
+merge = file1.merge(file2[['zohoname', 'grn']], on='zohoname', how='inner')
 
-f1 = pd.read_csv('zoho_auths.csv')
-f2 = pd.read_csv('zoho_auths1.csv')
-
-# Merge the DataFrames on the 'itemdesc' column
-merged_df = f1.merge(f2[['zohoname', 'grn']], on='zohoname', how='inner')
-
-# Replace the column in f1 with the corresponding column from f2
-merged_df['grn'] = merged_df['grn']
-
-# Drop the original 'column_to_replace' column
-merged_df.drop(columns=['grn'], inplace=True)
-
-# Save the modified DataFrame to a new CSV file
-merged_df.to_csv('merged_file.csv', index=False)
+# file1['grn'] = merge['grn']
+print(merge.head())
+# print(file2.head())
