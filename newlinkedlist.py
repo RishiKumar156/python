9daeb51f9d3ab0a2e5c168b21b0d0d7a3b7cf499
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-13 20:09:39 +0530

    linked in solution

--- newlinkedlist.py
+++ newlinkedlist.py
@@ -1,50 +1,77 @@
-class nodes():
-    def __init__(self, data):
-        self.data = data
-        self.pointer = None
+class node():
+    def __init__(self , data):
+        self.data = data 
+        self.pointer = None 
         
-
+    
 class LinkedList():
     def __init__(self , data):
-        c_node = nodes(data)
-        self.head = c_node
-        self.tail = c_node
+        create_node = node(data)
+        self.head = create_node
+        self.tail = create_node
         self.length = 1
+    
+    def print_nodes(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.pointer
         
-    def create_node(self,data):
-        c_node = nodes(data)
+    def add_node(self, data):
+        create_node = node(data)
         if self.head is None:
-            self.head = c_node
-            self.tail = c_node
+            self.head = create_node
+            self.tail = create_node
+        else :
+            self.tail.pointer = create_node
+            self.tail = create_node
+        self.length += 1
+    
+    def prepend_node(self, data):
+        create_node = node(data)
+        if self.head is None:
+            self.head = create_node
+            self.tail = create_node
         else:
-            self.tail.pointer = c_node
+            create_node.pointer = self.head
+            self.head = create_node
         self.length += 1
         return True
-    
-    def print_list(self):
-        curn_node = self.head
-        while curn_node is not None:
-            print(curn_node.data)
-            curn_node = curn_node.pointer
     
     def pp(self):
         if self.length == 0:
             return 0
-        c_n = self.head
-        pv_n = self.head
-        while c_n.pointer is not None:
-            pv_n = c_n
-            c_n = c_n.pointer
-        self.tail = pv_n
+        ct = self.head
+        pvt = ct
+        while ct.pointer is not None:
+            pvt = ct 
+            ct = ct.pointer
+        self.tail = pvt
         self.tail.pointer = None
         self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        k = print(c_n.data)
+        k = print(f'Poped item {ct.data}')
         return k
     
-mynode = LinkedList(345)
-mynode.create_node(786)
-mynode.pp()
-mynode.print_list()
+    def ppf(self):
+        if self.length == 0:
+            return 0 
+        first_item = self.head 
+        self.head = self.head.pointer
+        first_item.pointer = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        k = print(f'First node deleted {first_item.data}')
+        return k 
+        
+    def get_node(self , index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.pointer
+        return temp.data
+
+mynode = LinkedList(908)
+# mynode.get_node(0)
+print(f'The found node is {mynode.get_node(0)}')
