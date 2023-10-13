b26352e53889f7b6037bdd42c5db99463be0560e
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-13 20:51:03 +0530

    new linkedlist solutions

--- newlinkedlist.py
+++ newlinkedlist.py
@@ -1,46 +1,46 @@
-class node():
-    def __init__(self , data):
-        self.data = data 
-        self.pointer = None 
-        
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.pointer = None
     
 class LinkedList():
-    def __init__(self , data):
-        create_node = node(data)
-        self.head = create_node
-        self.tail = create_node
+    def __init__(self,data):
+        cn = Node(data)
+        self.head = cn 
+        self.tail = cn
         self.length = 1
     
-    def print_nodes(self):
-        current_node = self.head
-        while current_node is not None:
-            print(current_node.data)
-            current_node = current_node.pointer
+    def print_node(self):
+        node_item = self.head 
+        while node_item :
+            print(node_item.data)
+            node_item = node_item.pointer
         
-    def add_node(self, data):
-        create_node = node(data)
+    def prepend_node(self , data):
+        cn = Node(data)
         if self.head is None:
-            self.head = create_node
-            self.tail = create_node
-        else :
-            self.tail.pointer = create_node
-            self.tail = create_node
+            self.head = cn 
+            self.tail = cn 
+        else:
+            cn.pointer = self.head
+            self.head = cn 
         self.length += 1
     
-    def prepend_node(self, data):
-        create_node = node(data)
+    def add_node(self, data):
+        cn = Node(data)
         if self.head is None:
-            self.head = create_node
-            self.tail = create_node
-        else:
-            create_node.pointer = self.head
-            self.head = create_node
+            self.head = cn 
+            self.tail = cn 
+        else :
+            self.tail.pointer = cn 
+            self.tail = cn 
         self.length += 1
-        return True
     
     def pp(self):
         if self.length == 0:
-            return 0
+            return None
         ct = self.head
         pvt = ct
         while ct.pointer is not None:
@@ -49,41 +49,38 @@ class LinkedList():
         self.tail = pvt
         self.tail.pointer = None
         self.length -= 1
-        k = print(f'Poped item {ct.data}')
-        return k
+        return ct.data
     
     def ppf(self):
         if self.length == 0:
-            return 0 
-        first_item = self.head 
+            return None
+        ct = self.head
         self.head = self.head.pointer
-        first_item.pointer = None
+        ct.pointer = None
         self.length -= 1
-        if self.length == 0:
-            self.tail = None
-        k = print(f'First node deleted {first_item.data}')
-        return k 
-        
+        return ct.data
+    
     def get_node(self , index):
         if index < 0 or index >= self.length:
             return None
-        temp = self.head
+        ct = self.head
         for _ in range(index):
-            temp = temp.pointer
-        return temp.data
-    
-    def set_data(self , index, data):
-        if index < 0 or index >= self.length :
-            return None
-        current_datas = self.head 
-        for _ in range(index):
-            current_datas = current_datas.pointer
-        current_datas.data = data
-        return current_datas.data
+            ct = ct.pointer
+        return ct.data
 
-mynode = LinkedList(908)
-mynode.set_data(0 , 786)
-mynode.add_node(987)
-# mynode.get_node(0)
-# print(f'The found node is {mynode.get_node(0)}')
-mynode.print_nodes()
+    def set_node_data(self, index , data):
+        if index < 0 or index >= self.length:
+            return None
+        ct = self.head
+        for _ in range(index):
+            ct = ct.pointer
+        ct.data = data
+        return ct.data
+    
+
+mynode = LinkedList(678)
+mynode.add_node(90)
+mynode.prepend_node(786)
+print(f'Pop first node is {mynode.ppf()}')
+mynode.print_node()
+print(f'Get node is index {mynode.set_node_data(0, 786)}')
