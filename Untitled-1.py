0519c3492749f4d13ee13103d32717fb069d4378
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-15 16:17:50 +0530

    new solution

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,98 +1,138 @@
-class Node():
-    def __init__(self, data) -> None:
-        self.data = data
-        self.pointer = None
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
         
-class LinkedList():
-    def __init__(self, data) -> None:
-        cn = Node(data)
-        self.head = cn 
-        self.tail = cn 
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
         self.length = 1
-    
-    def print_nodes(self):
-        ttnodes = self.head
-        while ttnodes:
-            print(ttnodes.data)
-            ttnodes = ttnodes.pointer 
-    
-    def prepend(self, data):
-        cn = Node(data)
-        if not self.head:
-            self.head = cn 
-            self.tail = cn 
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
         else:
-            cn.pointer = self.head
-            self.head = cn 
+            self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
-        return cn 
-    
-    def append_node(self, data):
-        cn = Node(data)
-        if not self.head:
-            self.head = cn 
-            self.tail = cn 
-        else:
-            self.tail.pointer = cn
-            self.tail = cn
-        self.length += 1
-        return cn 
-    
-    def get_node(self, index):
-        if index < 0 or index >= self.length:
+        return True
+
+    def pop(self):
+        if self.length == 0:
             return None
-        tnode = self.head
-        for _ in range(index):
-            tnode = tnode.pointer
-        return tnode
-    
-    def set_node(self, index, data):
-        node = self.get_node(index)
-        if node:
-            node.data = data
-        return node
-    
-    def pop_node(self):
-        if self.length <= 0:
-            return None
-        ttnodes = self.head
-        previous_node = None
-        while ttnodes.pointer:
-            previous_node = ttnodes
-            ttnodes = ttnodes.pointer
-        if previous_node:
-            previous_node.pointer = None
-        else:
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
             self.head = None
             self.tail = None
-        self.length -= 1
-        return ttnodes
-    
-    def pop_first_node(self):
-        if self.length <= 0:
-            return None
-        tt_nodes = self.head
-        self.head = tt_nodes.pointer
-        if self.length == 1:
-            self.tail = None
-        self.length -= 1
-        return tt_nodes
-    
-    def insert_node(self, index, data):
-        if index < 0 or index > self.length:
-            return None
-        if index == 0:
-            return self.prepend(data)
-        if index == self.length:
-            return self.append_node(data)
-        
-        crnode = Node(data)
-        prev_node = self.get_node(index - 1)
-        crnode.pointer = prev_node.pointer
-        prev_node.pointer = crnode
-        self.length += 1 
-        return crnode
+        return temp
 
-mynode = LinkedList(897)
-mynode.append_node(7898)
-print(f'Removed node is {mynode.pop_node().data}')
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+        
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1   
+        return True  
+
+    ## WRITE REMOVE METHOD HERE ##
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None 
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop()
+        prev_item = self.get(index-1)
+        temp = prev_item.next 
+        prev_item.next = temp.next
+        temp.next = None
+        self.length -=  1 
+        return temp
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print('LL before remove():')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(2).value)
+print('LL after remove() in middle:')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(0).value)
+print('LL after remove() of first node:')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(2).value)
+print('LL after remove() of last node:')
+my_linked_list.print_list()
+
