bc1aa94c2300adb3207cebe4bc5c55323846577f
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-20 19:45:43 +0530

    linked list reverseal solution

--- test-reverse-ll.py
+++ test-reverse-ll.py
@@ -1,64 +1,50 @@
-class ListNode:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+class Node():
+    def __init__(self,data) -> None:
+        self.value = data 
+        self.next = None 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def append(self, value):
-        new_node = ListNode(value)
+class LinkedList():
+    def __init__(self,data) -> None:
+        create_node = Node(data)
+        self.head = create_node
+        self.length = 1
+    
+    def append_nodeS(self, data):
+        create_node = Node(data)
         if not self.head:
-            self.head = new_node
+            self.head = create_node
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.value, end=" -> ")
-            current = current.next
-        print("None")
-
-    def reverse_between_indices(self, left, right):
-        if left == right:
-            return
-
-        dummy = ListNode(0)
-        dummy.next = self.head
-        prev = dummy
-
-        # Move 'prev' to the node before the left index.
-        for _ in range(left - 1):
-            prev = prev.next
-        while prev:
-            print(prev.value)
+            tt = self.head 
+            while tt.next:
+                tt = tt.next 
+        tt.next = create_node
+        self.length += 1
+    
+    def print_nodes(self):
+        tt = self.head 
+        while tt:
+            print(tt.value)
+            tt = tt.next 
+    
+    def rever_between(self, start_index , end_index):
+        dummy = Node(0)
+        dummy.next = self.head 
+        prev = dummy 
+        
+        for _ in range(start_index):
             prev = prev.next 
-
-        current = prev.next
-        for _ in range(right - left):
-            next_node = current.next
-            current.next = next_node.next
-            next_node.next = prev.next
-            prev.next = next_node
-
-        # self.head = dummy.next
-
-# Example usage
-my_list = LinkedList()
-for val in [1, 2, 3, 4, 5]:
-    my_list.append(val)
-
-print("Original List:")
-my_list.print_list()
-
-left_index = 2
-right_index = 4
-my_list.reverse_between_indices(left_index, right_index)
-
-print("\nList after reversing from index", left_index, "to", right_index)
-my_list.print_list()
+        current_node = prev.next 
+        for _ in range(end_index-start_index):
+            next_node = current_node.next 
+            current_node.next = next_node.next 
+            next_node.next = prev.next 
+            prev.next  = next_node 
+        self.head = dummy.next 
+    
+mynode = LinkedList(1)
+mynode.append_nodeS(2)
+mynode.append_nodeS(3)
+mynode.append_nodeS(4)
+mynode.append_nodeS(5)
+mynode.rever_between(2,3)
+mynode.print_nodes()
