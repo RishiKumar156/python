0103782d66325f95c9214db278cb35cbcb6ad796
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-22 12:19:08 +0530

    new solution

---
+++ swap_first_last_dll.py
@@ -0,0 +1,20 @@
+"""swap first and last value in a linked list"""
+class Node:
+    def __init__(self,data) -> None:
+        self.value = data 
+        self.next = None 
+
+class LinkedList:
+    def __init__(self,data) -> None:
+        create_node = Node(data)
+        self.head = create_node
+        self.tail = create_node
+        self.length = 1
+    
+    def reverse_nodes(self):
+        if self.length == 0:
+            return None 
+        if self.length == 1:
+            return None 
+        self.head.value , self.tail.value = self.tail.value , self.head.value
+        return self.head 
