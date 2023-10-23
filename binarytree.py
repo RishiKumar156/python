f0e160f7828a27b96dc82071dc60bdda9f320c83
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-23 19:49:25 +0530

    Binary tree

--- binarytree.py
+++ binarytree.py
@@ -1,20 +1,18 @@
 class Node:
-    def __init__(self,data) -> None:
-        self.value = data
+    def __init__(self,value):
+        self.value = value 
         self.left = None 
         self.right = None 
 
-
-class BinaryTree:
+class Binarytree:
     def __init__(self) -> None:
         self.root = None 
-    
+
     def insert(self,value):
         create_node = Node(value)
         if not self.root:
-            self.root = create_node
-            return True 
-        temp = self.root
+            self.root = create_node 
+        temp = self.root 
         while True:
             if create_node.value == temp.value:
                 return None 
@@ -28,7 +26,12 @@ class BinaryTree:
                     temp.right = create_node
                     return True 
                 temp = temp.right 
-                
-mytree = BinaryTree()
+            
+mytree = Binarytree()
+mytree.insert(1)
+mytree.insert(2)
+mytree.insert(3)
 
-print(mytree.root)
+print(mytree.root.value)
+print(mytree.root.right.value)
+print(mytree.root.right.right.value)
