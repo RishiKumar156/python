479d1df8d0f9cfae2541211cd30f46d1db5c15ac
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-25 21:45:37 +0530

    new solution for maxheap

--- heap.py
+++ heap.py
@@ -1,52 +1,53 @@
-class MinHeap:
+class MaxHeap:
     def __init__(self) -> None:
         self.heap = []
-        
-    def _lef(self,index):
+    
+    def _left_child(self,index):
         return 2 * index + 1
     
-    def _right(self, index):
+    def _right_child(self,index):
         return 2 * index + 2 
     
-    def _swap(self, idx1 , idx2):
-        self.heap[idx1] , self.heap[idx2] = self.heap[idx2] , self.heap[idx1]
+    def _parent(self,index):
+        return (index - 1) // 2
     
-    def _parent(self , find_parent):
-        return (find_parent - 1) // 2
+    def _swap(self,index1 , index2):
+        self.heap[index1] , self.heap[index2] = self.heap[index2], self.heap[index1]
     
-    def _insert(self,value):
+    def _insert(self, value):
         self.heap.append(value)
         current_index = len(self.heap) - 1 
-        while current_index > 0 and self.heap[current_index] < self.heap[self._parent(current_index)]:
+        while current_index > 0 and self.heap[current_index] > self.heap[self._parent(current_index)]:
             self._swap(current_index , self._parent(current_index))
             current_index = self._parent(current_index)
-            
-    def _sink_down(self, index):
-        max_index = index
+    
+    def _sink_down(self,index):
+        incoming_index = index
+        left_index = self._left_child(index)
+        right_index = self._right_child(index)
         while True:
-            left_index = self._lef(index)
-            right_index = self._right(index)
-            if left_index < len(self.heap) and self.heap[left_index] < self.heap[max_index]:
-                max_index = left_index
-            if right_index < len(self.heap) and self.heap[right_index] < self.heap[max_index]:
-                max_index = right_index
-            if max_index != index:
-                self._swap(index, max_index)
-                index = max_index
+            if (left_index < len(self.heap) and self.heap[left_index] > self.heap[incoming_index]):
+                incoming_index = left_index
+            if (right_index < len(self.heap) and self.heap[right_index] > self.heap[incoming_index]):
+                incoming_index = right_index
+            if incoming_index != index :
+                self._swap(index , incoming_index)
+                index = incoming_index
             else:
-                return 
-
-def make_to_max_heap(nums , k ):
-    heap = MinHeap()
-    for i in range(k):
-        heap.heap.append(nums[i])
-    for i in range(k , len(nums)):
-        heap.heap[0] = nums[i]
-    return heap.heap
-mynode = MinHeap()
-mynode._insert(3)
-mynode._insert(4)
-mynode._insert(4)
-mynode._insert(5)
-# print(mynode.heap)
-print(make_to_max_heap([5,4,3,2] , 3))
+                return
+        
+    def _remove(self):
+        if len(self.heap) == 0:
+            return None 
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        first_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return first_value
+myheap = MaxHeap()
+myheap._insert(14)
+myheap._insert(1)
+myheap._insert(114)
+myheap._remove()
+print(myheap.heap)
