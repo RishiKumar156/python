68f120540d5f2158ab3c2f11c07e4362baa0fc85
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-27 21:59:19 +0530

    recursive

--- recursive_ex.py
+++ recursive_ex.py
@@ -11,3 +11,11 @@ def functhree():
 
 
 functhree()
+
+"""Factotail function recustsion this action should do the same thing repeatedly and should get reduced by the value theres is a recursion condition is to follow the same rules for each call's """
+def factorial(n):
+    if n== 1:
+        return 1 
+    return n * factorial(n-1)
+
+print(factorial(4))
