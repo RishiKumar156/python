8ad86aa088ece5192b22b3388a4c9479b03eba30
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-29 13:31:33 +0530

    Binary Search tree test complete

--- test-tree.py
+++ test-tree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,value) -> None:
+    def __init__(self, value) -> None:
         self.value = value
         self.left = None 
         self.right = None 
@@ -7,42 +7,37 @@ class Node:
 class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None 
-
-    def __r_insert(self,cnode,value):
-        if not cnode:
+    
+    def _insert(self, temp ,value):
+        if not temp:
             return Node(value)
-        if cnode.value == value:
-            return False 
-        if value > cnode.value:
-            cnode.right = self.__r_insert(cnode.right,value)
-        if value < cnode.value:
-            cnode.left = self.__r_insert(cnode.left,value)
-        return cnode
-        
-    def _insert(self,value):
+        if value > temp.value:
+            temp.right = self._insert(temp.right, value)
+        if value < temp.value:
+            temp.left = self._insert(temp.left, value)
+        return temp
+    
+    def _r_insert(self,value):
         if not self.root:
             self.root = Node(value)
-        return self.__r_insert(self.root,value)
+        return self._insert(self.root,value)
     
     def _r_contains(self,temp,value):
         if not temp:
-            return False
+            return False 
         if temp.value == value:
             return True
         if value > temp.value:
             return self._r_contains(temp.right, value)
         if value < temp.value:
             return self._r_contains(temp.left, value)
-        
+    
     def contains(self,value):
-        return self._r_contains(self.root,value)
+        return self._r_contains(self.root , value)
 
 mynode = BinarySearchTree()
-mynode._insert(5)
-mynode._insert(4)
-mynode._insert(3)
-mynode._insert(6)
-mynode._insert(6)
-mynode._insert(786)
-print(mynode.root.left.left.value)
-print(mynode.contains(7826))
+mynode._r_insert(5)
+mynode._r_insert(3)
+mynode._r_insert(786)
+print(mynode.root.right.value)
+print(mynode.contains(7861))
