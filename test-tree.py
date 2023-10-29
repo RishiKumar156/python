6820e24fbc823e12ff25d262c28eed81317e2f2c
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-10-29 13:05:30 +0530

    Binary Search Tree applications

--- test-tree.py
+++ test-tree.py
@@ -1,66 +1,30 @@
 class Node:
-    def __init__(self, value) -> None:
+    def __init__(self,value) -> None:
         self.value = value
-        self.left = None 
         self.right = None 
+        self.left = None 
     
 class BinarySearchTree:
     def __init__(self) -> None:
-        self.root = None 
+        self.root = None
     
-    def _insert(self,value):
-        new_node = Node(value)
+    def __r_insert(self,current_node, value):
+        if not current_node:
+            return Node(value) 
+        if value > current_node.value:
+            current_node.right =  self.__r_insert(current_node.right, value)
+        if value < current_node.value:
+            current_node.left =  self.__r_insert(current_node.left, value)
+        return current_node
+    
+    def _insert(self, value):
         if not self.root:
-            self.root = new_node
-        else:
-            temp = self.root
-            while temp:
-                if temp.value > new_node.value:
-                    if not temp.left:
-                        temp.left = new_node
-                        return
-                    temp = temp.left
-                if temp.value < new_node.value:
-                    if not temp.right:
-                        temp.right = new_node
-                        return
-                    temp = temp.right
-                    
-    def __r_contains(self,temp, value):
-        if not temp:
-            return False 
-        if temp.value == value:
-            return True
-        if value > temp.value:
-            return self.__r_contains(temp.right, value)
-        if value > temp.value:
-            return self.__r_contains(temp.left, value)
-    
-    def r_contains(self,value):
-        return self.__r_contains(self.root, value)
-    
-    def __r_insert(self, value):
-        new_node = Node(value)
-        temp = self.root
-        if not temp:
-            self.root = new_node
-            return
-        if new_node.value > temp.value:
-            return self.__r_insert(temp.right, value)
-        else:
-            if not temp.left:
-                temp.left = new_node
-                return
-            temp = temp.left
-            
-    def r_insert(self,value):
-        return self.__r_insert(value)
+            self.root = Node(value)
+        return self.__r_insert(self.root,value)
     
 mynode = BinarySearchTree()
 mynode._insert(3)
 mynode._insert(5)
+mynode._insert(4)
 mynode._insert(2)
-mynode._insert(786)
-mynode._insert(186)
-print(mynode.root.right.right.value)
-print(mynode.r_contains(3))
+print(mynode.root.left.value)
