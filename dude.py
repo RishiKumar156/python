61b73aeebeaa5a2949fa3804cbee9d6c7287f508
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-11-12 20:36:00 +0530

    new solutions updates

--- dude.py
+++ dude.py
@@ -5,14 +5,16 @@ def check_the_strak(box1,box2):
     hash_table = {}
     for i in range(len(box1)):
         hash_table[box1[i]] = hash_table.get(box1[i], 0) + 1
-        
+    
     for i in range(len(box2)):
         hash_table[box2[i]] = hash_table.get(box2[i], 0) + 1
-        
+    
     for k , v in hash_table.items():
         if v >=2 :
             print(k,v)
     return True
+
+
 
 
 print(check_the_strak(box1, box2))
